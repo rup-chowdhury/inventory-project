feeb21cfc46e57d7439831e1638633d61c91722f
Rup Chowdhury <rupbd1@gmail.com>
2026-03-19 23:22:21 +0100

    substraction between two integers

--- inventory-project/src/Main.java
+++ inventory-project/src/Main.java
@@ -1,9 +1,11 @@
-// Main Branch
+// Substract Branch
 // First Commit
 
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Main Branch and 1st Commit");
+        int result = SubstractNumbers.substract(10, 5);
+        System.out.println("Substract Branch and 1st Commit");
+        System.out.println(result);
     }
 }
